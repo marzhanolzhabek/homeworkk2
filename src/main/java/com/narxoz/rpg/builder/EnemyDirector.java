525d7efcc3e9551c5f688d47b87f31f605b21b7b
Marzhan <marzhan.olzhabek@narxoz.kz>
2026-02-19 16:31:09 +0500

    EnemyDirector.java

--- src/main/java/com/narxoz/rpg/builder/EnemyDirector.java
+++ src/main/java/com/narxoz/rpg/builder/EnemyDirector.java
@@ -1,7 +1,8 @@
 package com.narxoz.rpg.builder;
 
 import com.narxoz.rpg.enemy.Enemy;
-import com.narxoz.rpg.factory.EnemyComponentFactory;
+
+import java.util.List;
 
 public class EnemyDirector {
     private EnemyBuilder builder;
@@ -10,7 +11,8 @@ public class EnemyDirector {
         this.builder = builder;
     }
 
-    public Enemy createMinion(EnemyComponentFactory factory) {
+    
+    public Enemy createMinion(com.narxoz.rpg.factory.EnemyComponentFactory factory) {
         return builder.setName("Minion")
                 .setHealth(100)
                 .setDamage(20)
@@ -21,7 +23,8 @@ public class EnemyDirector {
                 .build();
     }
 
-    public Enemy createElite(EnemyComponentFactory factory) {
+    
+    public Enemy createElite(com.narxoz.rpg.factory.EnemyComponentFactory factory) {
         return builder.setName("Elite")
                 .setHealth(300)
                 .setDamage(50)
@@ -32,18 +35,66 @@ public class EnemyDirector {
                 .build();
     }
 
-    public Enemy createRaidBoss(EnemyComponentFactory factory) {
+
+    public Enemy createRaidBoss(com.narxoz.rpg.factory.EnemyComponentFactory factory) {
         if (builder instanceof BossEnemyBuilder) {
-            return ((BossEnemyBuilder) builder)
+            BossEnemyBuilder bossBuilder = (BossEnemyBuilder) builder;
+
+            return bossBuilder
                     .setName("Dragon Boss")
                     .setHealth(10000)
                     .setDamage(500)
                     .setDefense(200)
                     .setSpeed(20)
                     .setElement("Fire")
+                    .setPhaseCount(3)
+                    .setAiBehavior("Aggressive")
+                    .setEnrageMode(true)
+                    .setDifficultyMultiplier(2)
+                    .setBossLevel(10)
+                    .setIsRaidBoss(true)
+                    .setArmorType("Scale")
+                    .setResistances(List.of("Fire", "Ice"))
+                    .setRegion("Volcano")
+                    .setHasUltimate(true)
+                    .setUltimateCooldown(30)
+                    .setIsImmuneToStatus(false)
+                    .setSpecialMechanic("Tail Swipe")
                     .setComponents(factory)
                     .build();
         }
+
         throw new IllegalStateException("Builder must be BossEnemyBuilder for raid boss!");
+    }
+
+
+    public Enemy createMiniBoss(com.narxoz.rpg.factory.EnemyComponentFactory factory) {
+        if (builder instanceof BossEnemyBuilder) {
+            BossEnemyBuilder bossBuilder = (BossEnemyBuilder) builder;
+
+            return bossBuilder
+                    .setName("Mini Boss")
+                    .setHealth(2000)
+                    .setDamage(150)
+                    .setDefense(80)
+                    .setSpeed(15)
+                    .setElement("Ice")
+                    .setPhaseCount(2)
+                    .setAiBehavior("Defensive")
+                    .setEnrageMode(false)
+                    .setDifficultyMultiplier(1)
+                    .setBossLevel(5)
+                    .setIsRaidBoss(false)
+                    .setArmorType("Plate")
+                    .setResistances(List.of("Ice"))
+                    .setRegion("Glacier")
+                    .setHasUltimate(false)
+                    .setUltimateCooldown(0)
+                    .setIsImmuneToStatus(false)
+                    .setSpecialMechanic("Frost Breath")
+                    .setComponents(factory)
+                    .build();
+        }
+        throw new IllegalStateException("Builder must be BossEnemyBuilder for mini boss!");
     }
 }
