7c79810e72c96845b10bc8630ba86fe21f744869
Marzhan <marzhan.olzhabek@narxoz.kz>
2026-02-19 16:32:38 +0500

    DragonBoss.java

--- src/main/java/com/narxoz/rpg/enemy/DragonBoss.java
+++ src/main/java/com/narxoz/rpg/enemy/DragonBoss.java
@@ -5,14 +5,38 @@ import com.narxoz.rpg.loot.LootTable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class DragonBoss extends Enemy {
     private int phaseCount;
+    private Map<Integer, Integer> bossPhases;
 
     public DragonBoss(String name, int health, int damage, int defense, int speed, String element,
                       List<Ability> abilities, LootTable lootTable, int phaseCount) {
         super(name, health, damage, defense, speed, element, abilities, lootTable);
         this.phaseCount = phaseCount;
+    }
+
+
+    public DragonBoss(String name, int health, int damage, int defense, int speed, String element,
+                      List<Ability> abilities, LootTable lootTable, int phaseCount,
+                      String aiBehavior, boolean enrageMode, int difficultyMultiplier,
+                      int bossLevel, boolean isRaidBoss, String armorType, List<String> resistances,
+                      String region, boolean hasUltimate, int ultimateCooldown,
+                      boolean isImmuneToStatus, String specialMechanic) {
+        super(name, health, damage, defense, speed, element, abilities, lootTable);
+        this.phaseCount = phaseCount;
+      
+        this.bossPhases = null; 
+  
+    }
+
+    public void setBossPhases(Map<Integer, Integer> bossPhases) {
+        this.bossPhases = bossPhases;
+    }
+
+    public Map<Integer, Integer> getBossPhases() {
+        return bossPhases;
     }
 
     public int getPhaseCount() {
@@ -26,12 +50,19 @@ public class DragonBoss extends Enemy {
             clonedAbilities.add(a.clone());
         }
         LootTable clonedLoot = lootTable.clone();
-        return new DragonBoss(name, health, damage, defense, speed, element, clonedAbilities, clonedLoot, phaseCount);
+
+        DragonBoss clone = new DragonBoss(name, health, damage, defense, speed, element, clonedAbilities, clonedLoot, phaseCount);
+        clone.setBossPhases(bossPhases); 
+        return clone;
     }
 
     @Override
     public void displayStats() {
         super.displayStats();
         System.out.println("Phases: " + phaseCount);
+        if (bossPhases != null) {
+            System.out.println("Phase thresholds:");
+            bossPhases.forEach((phase, hp) -> System.out.println("Phase " + phase + ": " + hp + " HP"));
+        }
     }
 }
